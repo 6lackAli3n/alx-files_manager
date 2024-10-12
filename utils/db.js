805891a57a5b06f3d7bab7c8c893405e64025b0f
6lackAli3n <abubakarabdullahayo26@gmail.com>
2024-10-13 00:32:21 +0100

    db

--- utils/db.js
+++ utils/db.js
@@ -5,11 +5,11 @@ dotenv.config();
 
 class DBClient {
 	constructor() {
-
 		const host = process.env.DB_HOST || 'localhost';
 		const port = process.env.DB_PORT || 27017;
 		const database = process.env.DB_DATABASE || 'files_manager';
 		const url = `mongodb://${host}:${port}`;
+		
 		this.client = new MongoClient(url, { useUnifiedTopology: true });
 		this.client.connect()
 		.then(() => {
