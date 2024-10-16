3b197c1e00baf36ee029542966fae287f75265b9
6lackAli3n <abubakarabdullahayo26@gmail.com>
2024-10-16 13:43:19 +0100

    update

---
+++ controllers/UsersController.js
@@ -0,0 +1,38 @@
+import { ObjectId } from 'mongodb';
+import crypto from 'crypto';
+import dbClient from '../utils/db';
+
+class UsersController {
+	static async postNew(req, res) {
+		const { email, password } = req.body;
+
+		if (!email) {
+			return res.status(400).json({ error: 'Missing email' });
+		}
+
+		if (!password) {
+			return res.status(400).json({ error: 'Missing password' });
+		}
+
+		const userExists = await dbClient.db.collection('users').findOne({ email });
+		if (userExists) {
+			return res.status(400).json({ error: 'Already exist' });
+		}
+
+		const sha1Password = crypto.createHash('sha1').update(password).digest('hex');
+
+		const newUser = {
+			email,
+			password: sha1Password,
+		};
+
+		const result = await dbClient.db.collection('users').insertOne(newUser);
+
+		return res.status(201).json({
+			id: result.insertedId,
+			email: newUser.email,
+		});
+	}
+}
+
+export default UsersController;
--- utils/db.js
+++ utils/db.js
@@ -1,41 +1,49 @@
-const { MongoClient } = require('mongodb');
+import { MongoClient } from 'mongodb';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 class DBClient {
 	constructor() {
 		const host = process.env.DB_HOST || 'localhost';
 		const port = process.env.DB_PORT || 27017;
 		const database = process.env.DB_DATABASE || 'files_manager';
-		const uri = `mongodb://${host}:${port}/${database}`;
-		
-		this.client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-		this.connected = false;
+		const url = `mongodb://${host}:${port}`;
+
+		this.client = new MongoClient(url, { useUnifiedTopology: true });
+		this.client.connect()
+		.then(() => {
+			this.db = this.client.db(database);
+		})
+		.catch((err) => {
+			console.error('MongoDB connection error:', err);
+		});
 	}
 
-	async connect() {
-		if (!this.connected) {
-			await this.client.connect();
-			this.connected = true;
-		}
-	}
-
+	/**
+	 * Check if the MongoDB client is alive (connected).
+	 * @returns {boolean} true if connected, false otherwise.
+	 */
 	isAlive() {
-		return this.connected;
+		return this.client.isConnected();
 	}
 
+	/**
+	 * Get the number of users in the users collection.
+	 * @returns {Promise<number>} The number of users.
+	 */
 	async nbUsers() {
-		await this.connect();
-		const db = this.client.db();
-		const usersCount = await db.collection('users').countDocuments();
-		return usersCount;
+		return this.db.collection('users').countDocuments();
 	}
 
+	/**
+	 * Get the number of files in the files collection.
+	 * @returns {Promise<number>} The number of files.
+	 */
 	async nbFiles() {
-		await this.connect();
-		const db = this.client.db();
-		const filesCount = await db.collection('files').countDocuments();
-		return filesCount;
+		return this.db.collection('files').countDocuments();
 	}
 }
 
 const dbClient = new DBClient();
-module.exports = dbClient;
+export default dbClient;
